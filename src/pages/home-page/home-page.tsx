0db9581085b861de28104be93f99d376fa81dc02
lahiruvimukthi292 <lahiruvimukthi292@gmail.com>
2025-02-19 09:53:23 +0000

    add

--- src/pages/home-page/home-page.tsx
+++ src/pages/home-page/home-page.tsx
@@ -19,19 +19,19 @@ const HomePage: React.FC = () => {
     return (
         <div>
             <NavBar />
-            <Box sx={{ width: '90%', margin: 'auto',paddingTop:'55px', mt: 4 }}>
+            <Box sx={{ width: '85%', margin: 'auto',paddingTop:'55px', mt: 4 }}>
                 <Slider {...settings}>
                     <div>
-                        <img src="https://plus.unsplash.com/premium_photo-1673264933048-3bd3f5b86f9d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
-                        alt="Slide 1" style={{ width: '100%',maxHeight:'65vh', borderRadius: 8 }} />
+                        <img src="https://plus.unsplash.com/premium_photo-1679428402040-e3c93439ec13?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+                        alt="Slide 1" style={{ width: '100%',maxHeight:'75vh', borderRadius: 8 }} />
                     </div>
                     <div>
-                        <img src="https://plus.unsplash.com/premium_photo-1673264933048-3bd3f5b86f9d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
-                        alt="Slide 2" style={{ width: '100%',maxHeight:'65vh',  borderRadius: 8 }} />
+                        <img src="https://images.unsplash.com/photo-1581093577421-f561a654a353?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+                        alt="Slide 2" style={{ width: '100%',maxHeight:'75vh',  borderRadius: 8 }} />
                     </div>
                     <div>
-                        <img src="https://plus.unsplash.com/premium_photo-1673264933048-3bd3f5b86f9d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                         alt="Slide 3" style={{ width: '100%',maxHeight:'65vh',  borderRadius: 8 }} />
+                        <img src="https://images.unsplash.com/photo-1425913397330-cf8af2ff40a1?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3Dhttps://images.unsplash.com/photo-1516214104703-d870798883c5?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                         alt="Slide 3" style={{ width: '100%',maxHeight:'75vh',  borderRadius: 8 }} />
                     </div>
                 </Slider>
             </Box>

fdac640ffe23c427c5356cc0a91a0698d43c8e8d
Lahiru Vimukthi <67822002+LahiruV@users.noreply.github.com>
2025-02-26 18:37:03 +0530

    refactor: improve code formatting and readability in DetailComponent

--- src/components/home-components/detail-component.tsx
+++ src/components/home-components/detail-component.tsx
@@ -3,40 +3,39 @@ import { Card, CardActionArea, CardContent, CardMedia, Typography } from '@mui/m
 import React from 'react';;
 
 export interface DetailComponentProps {
-title?: string;
-description?: string;
-image?: string;
-isImage?:boolean;
+    title?: string;
+    description?: string;
+    image?: string;
+    isImage?: boolean;
 }
 
-const DetailComponent: React.FC<DetailComponentProps> = ({title,description,image,isImage}: DetailComponentProps) => {
+const DetailComponent: React.FC<DetailComponentProps> = ({ title, description, image, isImage }: DetailComponentProps) => {
 
     return (
         <div>
-            {isImage?
-            <Card >
-            <CardActionArea>
-            <CardMedia
-        sx={{ height: 437 }}
-        image={image || ''}
-        title="green iguana"
-      />
-            </CardActionArea>
-        </Card>:
-        <Card >
-        <CardActionArea>
-            <CardContent sx={{ height: 407 }}>
-                <Typography gutterBottom  variant="h5" component="div" className='underline font-28'>
-                    {title}
-                </Typography>
-                <br/>
-                <Typography className='font-16' sx={{ color: 'text.secondary' }}>
-                   {description}
-                </Typography>
-            </CardContent>
-        </CardActionArea>
-    </Card>
-        }
+            {isImage ?
+                <Card >
+                    <CardActionArea>
+                        <CardMedia
+                            sx={{ height: 437 }}
+                            image={image || ''}
+                        />
+                    </CardActionArea>
+                </Card> :
+                <Card >
+                    <CardActionArea>
+                        <CardContent sx={{ height: 407 }}>
+                            <Typography gutterBottom variant="h5" component="div" className='underline font-28'>
+                                {title}
+                            </Typography>
+                            <br />
+                            <Typography className='font-16' sx={{ color: 'text.secondary' }}>
+                                {description}
+                            </Typography>
+                        </CardContent>
+                    </CardActionArea>
+                </Card>
+            }
         </div>
     );
 };
